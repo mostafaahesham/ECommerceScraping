c85aeb6c681ebc8c1c2cc820f7a21bb025f66208
Omar Abou EL-Magd <omarislam97@gmail.com>
2022-12-04 22:10:34 +0200

    update zara

--- Zara/zara_parse.py
+++ Zara/zara_parse.py
@@ -1,10 +1,20 @@
 import pandas as pd
 import json
 
-df = pd.read_json('zara_men_content.json')
-brand = "Zara"
+from sqlalchemy import false, true
 
+df = pd.read_json('Zara\zara_men_content.json')
+brand = "Zara"
+img_header1 = "https://static.zara.net/photos//"
+img_header2 = "/"
+img_footer = ".jpg?ts="
 products = []
+_availability = False
+url_header = "https://www.zara.com/eg/en/"
+url_mid = "-p"
+url_footer = ".html"
+
+
 for page in df['productGroups']:
     for item in page:
         for elem in item['elements']:
@@ -15,19 +25,21 @@ for page in df['productGroups']:
                     for names in elem['commercialComponents']:
                         if(names['name'] != ''):
                                 pics = []
-                                header1 = "https://static.zara.net/photos//"
-                                header2 = "/w/293/"
-                                footer = ".jpg?ts="
                                 for image in names['xmedia']:
-                                    pics.append(header1 + image['path'] + header2 + image['name'] + footer + image['timestamp'])
+                                    pics.append(img_header1 + image['path'] + img_header2 + image['name'] + img_footer + image['timestamp'])
                                 if('oldPrice' in names.keys()):
                                     oldPrice = names['oldPrice']
                                 else:
-                                    oldPrice = ''
+                                    oldPrice = 0
                                 if('displayDiscountPercentage' in names.keys()):
                                     discountPercentage = names['displayDiscountPercentage']
                                 else: 
-                                    discountPercentage = ''
+                                    discountPercentage = 0
+                                
+                                if names['availability'] == "in_stock":
+                                    _availability = True
+                                
+                                item_url = url_header + names["seo"]["keyword"] + url_mid + names["seo"]["seoProductId"] + url_footer
                                     
                                 product = {
                                     "item_brand" : brand,
@@ -38,14 +50,29 @@ for page in df['productGroups']:
                                     "item_new_price" : names['price'],
                                     "item_old_price" : oldPrice,
                                     "item_discount" : discountPercentage,
-                                    "availability" : names['availability'],
-                                    "images" : pics
+                                    "item_link" : item_url,
+                                    "item_options": [
+                                        {
+                                            "color" : names["detail"]["colors"][0]["name"],
+                                            "color_code" : names["detail"]["colors"][0]["id"],
+                                            # "color_image" : 
+                                            "images" : pics,
+                                            "sizes": [
+                                                        {
+                                                            "name" : None,
+                                                            "availability":  _availability
+                                                        }
+                                                     ]
+                                        }
+                                        ]
+                                   
+                                    
 
                                 }
                                 products.append(product)
                                 break
             except:
-                print(names)
+                print(names["name"])
               
 
 # df = pd.read_json('zara_women_content.json')
@@ -83,6 +110,7 @@ for page in df['productGroups']:
 #             except:
 #                 print(names)
               
+
 
 json_object = json.dumps(products,indent=4)
 with open('zara_sample.json','w') as out:
