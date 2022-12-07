63d03710053c71a7361262d3e7eeb80ff309b773
mostafa <mostafaaheshamm@gmail.com>
2022-12-07 22:41:56 +0200

    Dalydress to desired database format

---
+++ Dalydress/dalydress_db.py
@@ -0,0 +1,156 @@
+import requests
+import json
+import os
+import time
+import math
+import numpy as np
+import pandas as pd
+
+brand_name = "Dalydress"
+
+main_url = "https://dalydress.com/products/"
+db_url = "https://xytpiqsykygz2k2wtthklmcu-fast.searchtap.net/v2"
+
+db_file_name = "dalydress_stock.json"
+sample_file_name = "dalydress_sample.json"
+links_file_name = "links.json"
+
+main_path = os.path.dirname(os.path.realpath(__file__))
+
+links_file_path = os.getcwd() + "/" + links_file_name
+db_file_path = main_path + "/" + db_file_name
+sample_file_path = main_path + "/" + sample_file_name
+
+with open(links_file_path,encoding='UTF-8') as sections_file:    
+    sections = json.load(sections_file)[brand_name] 
+    
+headers = {"authorization": "Bearer 2DYJG7BEJD5BCAW1Q9Z3NLHD"}
+    
+for section in sections:
+    for category in sections[section]:
+        response = requests.post(db_url,headers=headers,json = sections[section][category])
+        if response.status_code == 200:
+            print('Get "{} {}" Initial Request Status Code'.format(section,category), response.status_code)
+            item_count = response.json()['totalHits']
+            print('{} Items Found in "{} {}" Section'.format(item_count,section,category))
+            if item_count > 30:
+                    with open(sample_file_path, "w",encoding="utf-8") as outfile:
+                        outfile.write(json.dumps(response.json(), indent=4,ensure_ascii = False))
+                    print("{} {} Sample Saved".format(section,category))
+                    break
+
+            else:
+                continue
+        else:
+            print("{} {} bad request".format(section,category))
+    break
+                
+print("-------------------------------------------")
+
+unmerged_stock = []
+ids = []
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+for section in sections:
+    for category in sections[section]:
+        response = requests.post(db_url,headers=headers,json = sections[section][category])
+        if response.status_code == 200:
+            print('Get "{} {}" Initial Request Status Code'.format(section,category), response.status_code)
+            item_count = response.json()['totalHits']
+            print('{} Items Found in "{} {}" Section'.format(item_count,section,category))
+            category_items = response.json()['results']
+            for item in category_items:
+                try:
+                    id = item['handle'].split('-')[:-1]
+                    color_code = item['handle'].split('-')[-1]
+                    
+                    item_id = int(''.join(id))
+
+                    if item_id not in ids:
+                        ids.append(item_id)
+                    else:
+                        pass
+                    
+                    unmerged_stock.append(
+                                    {
+                                        'item_brand': brand_name,
+                                        'item_section': section,
+                                        'item_category': category[:-2],
+                                        'item_id': item_id,
+                                        'item_name': item['title'],
+                                        'item_on_sale': False if item['discount'] == 0 else True,
+                                        'item_old_price': math.floor(item['compare_at_price']),
+                                        'item_new_price': math.floor(item['price']),
+                                        'item_discount': math.floor(item['discount']),
+                                        'item_link': main_url + item['handle'],
+                                        'color': item['option_auto_color'][0].title(),
+                                        'color_code': color_code,
+                                        'color_image': item['image']['src'],
+                                        'images': [img['src'] for img in item['images']],
+                                        'sizes': [size.upper() for size in item['option_auto_size']]                           
+                                    }
+                            )
+                except Exception as e:print(e)
+        else:
+            print("{} {} Bad Request".format(section,category))
+            
+print('--------------------Merging Data---------------------')
+            
+df = pd.DataFrame(unmerged_stock)
+stock = []
+
+for id in ids:
+    item = df.loc[df['item_id'] == id]
+    options = item[['color','color_code','color_image','images','sizes']]
+
+    item_options = []
+
+    for i in range(len(options)):
+        option = {
+                "color": options.iloc[i]['color'],
+                "color_code": options.iloc[i]['color_code'],
+                "color_image": options.iloc[i]['color_image'],
+                "images": options.iloc[i]['images'],
+                "sizes": options.iloc[i]['sizes']
+            }
+        item_options.append(option)
+    try:        
+        stock.append(
+            {
+                'item_brand': item.iloc[0]['item_brand'],
+                'item_section': item.iloc[0]['item_section'],
+                'item_category': item.iloc[0]['item_category'],
+                'item_id': item.iloc[0]['item_id'],
+                'item_name': item.iloc[0]['item_name'],
+                'item_on_sale': bool(item.iloc[0]['item_on_sale']),
+                'item_old_price': item.iloc[0]['item_old_price'],
+                'item_new_price': item.iloc[0]['item_new_price'],
+                'item_discount': item.iloc[0]['item_discount'],
+                'item_link': item.iloc[0]['item_link'],
+                'item_options': item_options
+            }
+        )
+    except Exception as e:print(e)
+    
+for item in stock:
+    try:
+        item['default_option'] = {
+            "default_color": item['item_options'][0]['color'],
+            "default_color_code": item['item_options'][0]['color_code'],
+            "default_size": item['item_options'][0]['sizes'][0],
+            "default_image": item['item_options'][0]['images'][0],           
+        }
+    except Exception as e:print(e,item['item_id'])
+
+with open(db_file_path, "w", encoding='utf-8') as outfile:
+    outfile.write(json.dumps(stock, indent=4,ensure_ascii = False,cls=NpEncoder))
+
--- b3basa.py
+++ b3basa.py
@@ -3,28 +3,12 @@ from requests.structures import CaseInsensitiveDict
 
 url = "https://xytpiqsykygz2k2wtthklmcu-fast.searchtap.net/v2"
 
-headers = CaseInsensitiveDict()
-headers["Accept"] = "application/json, text/plain, */*"
-headers["Accept-Encoding"] = "gzip, deflate, br"
-headers["Accept-Language"] = "en-US,en;q=0.9"
-headers["authorization"] = "Bearer 2DYJG7BEJD5BCAW1Q9Z3NLHD"
-headers["Connection"] = "keep-alive"
-headers["Content-Length"] = "968"
-headers["Content-Type"] = "application/json"
-headers["Host"] = "xytpiqsykygz2k2wtthklmcu-fast.searchtap.net"
-headers["Origin"] = "https://dalydress.com"
-headers["Referer"] = "https://dalydress.com/"
-headers["sec-ch-ua"] = ""Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24""
-headers["sec-ch-ua-mobile"] = "?0"
-headers["sec-ch-ua-platform"] = ""Linux""
-headers["Sec-Fetch-Dest"] = "empty"
-headers["Sec-Fetch-Mode"] = "cors"
-headers["Sec-Fetch-Site"] = "cross-site"
-headers["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+headers = {"authorization": "Bearer 2DYJG7BEJD5BCAW1Q9Z3NLHD"}
 
-data = '{"query":"","fields":["*"],"textFacets":["system_producttype","system_vendor","system_availability","option_auto_color","option_auto_size","tags_gender_mhspyaq371nqa9w8y4ab3csl"],"highlightFields":[],"searchFields":["title","description","collections","tags","productType","vendor","variants.sku","sku"],"filter":"priorityScore >= 0 AND publishedTimestamp < 1669831286367 AND publishedTimestamp > 0 AND collectionHandles = \"mens-t-shirts-fw22\"  AND in_stock = 1","sort":["-publishedTimestamp"],"skip":0,"count":24,"collection":"RMW65H9J527XMB7EDYAJY11H","facetCount":100,"groupCount":-1,"typoTolerance":1,"textFacetFilters":{"system_producttype":[],"system_vendor":[],"system_availability":[],"price_price_filter":[],"option_auto_color":[],"option_auto_size":[],"tags_gender_mhspyaq371nqa9w8y4ab3csl":[]},"numericFacets":{"price":["[0,100)","[100,200)","[200,300)","[300,400)","[400,500)","[500,2147483647)"]},"numericFacetFilters":{},"textFacetQuery":null,"geo":{}}'
+data = {"query":"","fields":["*"],"textFacets":["system_producttype","system_vendor","system_availability","option_auto_color","option_auto_size","tags_gender_mhspyaq371nqa9w8y4ab3csl"],"highlightFields":[],"searchFields":["title","description","collections","tags","productType","vendor","variants.sku","sku"],"filter":"priorityScore >= 0 AND publishedTimestamp < 1670436056720 AND publishedTimestamp > 0 AND collectionHandles = \"all-mens-garments-aw22\"  AND in_stock = 1","sort":["-publishedTimestamp"],"skip":39,"count":39,"collection":"RMW65H9J527XMB7EDYAJY11H","facetCount":100,"groupCount":-1,"typoTolerance":1,"textFacetFilters":{"system_producttype":[],"system_vendor":[],"system_availability":[],"price_price_filter":[],"option_auto_color":[],"option_auto_size":[],"tags_gender_mhspyaq371nqa9w8y4ab3csl":[]},"numericFacets":{"price":["[0,100)","[100,200)","[200,300)","[300,400)","[400,500)","[500,2147483647)"]},"numericFacetFilters":{},"textFacetQuery":None,"geo":{}}
 
 
-resp = requests.post(url, headers=headers, data=data)
+resp = requests.post(url, headers = headers, json=data)
 
-print(resp.status_code)
+print(resp)
+
